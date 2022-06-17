2abdfbd467f5a75f28bf6f0ddea6517ff9902d51
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-18 02:50:20 +0900

    2022-06-18-0245

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/**
+ *@title WinFormGUI / Program.cs
+ *@content デフォルト Main()
+ */
+
+/// <summary>
+/// アプリケーションのメイン エントリ ポイントです。
+/// </summary>
+
+/// エントリーポイントを自己定義する場合は 
+/// static class, [STAThread],static Main()をコメントアウトし、
+/// public Main()に変更
+
+using System;
+using System.Windows.Forms;
+
+namespace WinFormGUI
+{
+    //static class Program
+    class Program
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }//Main()
+    }//class
+}
