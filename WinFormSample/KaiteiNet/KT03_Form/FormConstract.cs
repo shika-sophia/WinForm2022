0c91e833008bb303c253750e02a0eee9df1ccd18
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-19 18:02:40 +0900

    2022-06-19-1800

---
+++ WinFormSample/KaiteiNet/KT03_Form/FormConstract.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT03_Form
+{
+    class FormConstract : Form
+    {
+        public FormConstract()
+        {
+            this.Text = "FormConstract";
+            this.MaximizeBox = true;
+            this.MinimizeBox = true;
+            this.ControlBox = true;
+            this.TopMost = true;
+        }
+    }//class
+}
