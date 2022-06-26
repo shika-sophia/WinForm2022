b844489327799866ebea60c64619fc2eacef2432
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-26 19:57:02 +0900

    2022-06-26-2000

---
+++ CsharpCode/MainTemplate.cs
@@ -0,0 +1,54 @@
+﻿//==== Document Template ====
+
+/** 
+ *@title WinFormGUI / WinFormSample /
+ *@class 
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2010
+ *           http://kaitei.net/csforms/ 
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm_.txt〕
+ *           
+ *@content 
+ *@subject
+ *
+ *@see 
+ *@author shika
+ *@date 
+ */
+
+/*
+//==== Main() Template / Form class ====
+
+    [STAThread]
+    static void Main()
+    //public void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.Run(new Form1());
+    }//Main()
+}//class
+
+class Form1 : Form
+{
+    public Form1()
+    {
+        this.Text = "Form1";
+
+    }//constructor
+}//class
+
+ */
+namespace WinFormGUI.CsharpCode
+{
+    class MainTemplate
+    {
+        //static void Main(string[] args)
+        public void Main(string[] args)
+        {
+            // Csharp2022 / CsharpBeginプロジェクト (未参照)
+            // new Utility.FileDocumentDiv.FileDocExecute().ReadWriteExe();
+        }//Main()
+    }//class
+}
---
+++ CsharpCode/ShowEnumValue.cs
@@ -0,0 +1,110 @@
+﻿/*
+ *@title WinFormGUI / CsharpCode / ShowEnumValue.cs
+ *@content enumの 全列挙定数を表示する
+ *
+ *@subject foreach (object value in Enum.GetValues(enumType))
+ *         object[]     Enum.GetValues(Type)
+ *         string value       //value名の文字列表現
+ *         int    (int) value //valueの数値表現
+ *
+ *@subject Type typeof(Xxxx) //Xxxxは変数不可
+ *         Type xxxx.GetType()
+ *         string type.Name    クラス名
+ *         
+ *@subject 
+ *
+ *@NOTE【実行】WinForm2022のプロパティ -> [アプリケーション]タブ
+ *            -> 出力の種類: [コンソールアプリケーション]に変更
+ *            
+ *@author shika
+ *@date 2022-06-26
+ */
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormGUI.CsharpCode
+{
+    class ShowEnumValue
+    {
+        private readonly string enumName;
+        private readonly Type enumType;
+
+        public ShowEnumValue()
+        {
+            throw new ArgumentNullException(
+                    "'new ShowEnumValue(Type)' require its argument as like 'typeof(Xxxx)'");
+        }
+
+        public ShowEnumValue(Type enumType)
+        {
+            this.enumName = enumType.Name;
+            this.enumType = enumType;
+            CheckEnum(enumType);
+        }
+
+        private void CheckEnum(Type enumType)
+        {
+            try
+            {
+                Enum.GetValues(enumType);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.GetType());
+                Console.WriteLine(
+                    $"The Constructor Argument: typeof(Xxxx)\n" +
+                    $"Xxxx: {enumName}\n" +
+                    $"Xxxx should be Enum type.");
+            }
+        }
+
+        private string BuildEnumContent(Type enumType)
+        {
+            int length = Enum.GetNames(enumType).Length * 20;
+
+            var bld = new StringBuilder(length);
+            bld.Append($"enum {enumName}\n");
+            bld.Append("{\n");
+
+            foreach (var value in Enum.GetValues(enumType))
+            {
+                bld.Append($"    {value} = {(int)value},\n");
+            }//foreach
+            bld.Append("}\n");
+
+            //Console.WriteLine($"length:{length}");
+            //Console.WriteLine($"bld.Length:{bld.Length}");
+            return bld.ToString();
+        }
+
+        //==== Test Main() ====
+        //static void Main()
+        public void Main()
+        {
+            var here = new ShowEnumValue(typeof(MouseButtons));
+            string content = here.BuildEnumContent(here.enumType);
+            Console.WriteLine(content);
+        }//Main()
+    }//class
+}
+
+/*
+enum MessageBoxButtons
+{
+    OK = 0,
+    OKCancel = 1,
+    AbortRetryIgnore = 2,
+    YesNoCancel = 3,
+    YesNo = 4,
+    RetryCancel = 5,
+}
+
+System.ArgumentNullException: 
+'new ShowEnumValue(Type)' require its argument as like 'typeof(Xxxx)'
+
+System.ArgumentException
+The Constructor Argument: typeof(Xxxx)
+Xxxx: MouseEventArgs
+Xxxx should be Enum type.
+*/
