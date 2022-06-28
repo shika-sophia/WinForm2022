44d00cebbaa6d42e7d2c4a25a651ccc416252863
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-28 20:27:43 +0900

    2022-06-28-2030

---
+++ WinFormSample/KaiteiNet/KT05_Event/MainEventOverride.cs
@@ -0,0 +1,69 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / KaiteiNet / KT05_Event
+ *@class MainEventOverride.cs
+ *@class FormEventOverride.cs
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2010
+ *           http://kaitei.net/csforms/events/
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm05_Event.txt〕
+ *           
+ *@content KT 5. Event / EventHandler override
+ *@subject ◆イベントハンドラの override
+ *         ・各コントロールには OnXxxx(XxxxEventArgs) という既製のイベントハンドラーが用意されている。
+ *         ・Xxxxには イベント名が入る
+ *         ・overrideする場合は control.Xxxx += new XxxxEventHandler();
+ *           を行ってイベント追加する必要がない。
+ *         ・override時に必ず base.OnXxxx(e); を記述し、基底クラスの同メソッドを起動する
+ *
+ *         ＊メソッド
+ *         void control.OnClick(EventArgs)
+ *         void control.OnMouseClick(MouseEventArgs)
+ *           : など
+ *           
+ *@see 
+ *@author shika
+ *@date 2022-06-28
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT05_Event
+{
+    class MainEventOverride
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FormEventOverride());
+        }//Main()
+    }//class
+
+    class FormEventOverride : Form
+    {
+        private Label label;
+
+        public FormEventOverride()
+        {
+            this.Text = "FormEventOverride";
+            this.label = new Label()
+            {
+                Text = "Click this Form.",
+                Location = new Point(10, 10),
+                AutoSize = true,
+            };
+            this.Controls.Add(label);
+        }//constructor
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            MessageBox.Show("Clicked", "Clicked");
+        }
+    }//class
+
+}
---
+++ WinFormSample/KaiteiNet/KT05_Event/MainRepaintSample.cs
@@ -0,0 +1,96 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / KaiteiNet / KT05_Event
+ *@class MainRepaintSample.cs
+ *@class FormRepaintSample.cs
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2010
+ *           http://kaitei.net/csforms/events/ 
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm05_Event.txt〕
+ *           
+ *@content KT 5. Event / Paintイベント
+ *@subject ◆Paintイベント
+ *         再描画イベント repaint: パラメータや画面に変化があったとき、
+ *                               再び表示し直すイベントが発生する。
+ *                               
+ *        【再描画イベントが発生するデフォルト仕様】
+ *        ・Formが初めて表示されたとき
+ *        ・Formのサイズが拡大されたとき
+ *        ・最小化していた状態から、Formが再表示されるとき
+ *        ・control.Invalidate() 再描画の命令を出されたとき
+ *        
+ *        ＊イベント
+ *        PaintEventHandler control.Paint
+ *         
+ *        ＊イベントハンドラ
+ *        delegate void PaintEventHamdler(object sender, PaintEventArgs e)
+ *         
+ *        control.Paint += new PaintEventHandler(form_Pain);
+ *         
+ *        ＊Control共通メソッド
+ *         control.Invalidate() 現在のコントロール表示を無効にし、再描画を促す
+ *
+ *@see FormRepaintSample.jpg
+ *@author shika
+ *@date 2022-06-28
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT05_Event
+{
+    class MainRepaintSample
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FormRepaintSample());
+        }//Main()
+    }//class
+
+    class FormRepaintSample : Form
+    {
+        private Timer timer;
+        private Label label;
+        private int paintCount = 0;
+
+        public FormRepaintSample()
+        {
+            this.Text = "FormRepaintSample";
+
+            this.timer = new Timer()
+            {
+                Interval = 1000,
+                Enabled = true,
+            };
+
+            this.label = new Label()
+            {
+                Location = new Point(10, 10),
+                AutoSize = true,
+            };
+            this.Controls.Add(label);
+
+            timer.Tick += new EventHandler(form_Tick);
+            this.Paint += new PaintEventHandler(form_Paint);
+        }//constructor
+
+        private void form_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void form_Paint(object sender, PaintEventArgs e)
+        {
+            paintCount++;
+            label.Text = 
+                $"Repaint: {paintCount} times\n" +
+                $"Evented: {DateTime.Now.ToLongTimeString()}\n";
+        }
+    }//class
+
+}
