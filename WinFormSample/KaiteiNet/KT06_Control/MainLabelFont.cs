c1af541115f8c74fc96fedda71ade994c0ba8308
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-06-30 16:25:35 +0900

    2022-06-30-1620

--- WinFormSample/KaiteiNet/KT06_Control/MainLabelFont.cs
+++ WinFormSample/KaiteiNet/KT06_Control/MainLabelFont.cs
@@ -10,7 +10,49 @@
  *           =>〔~/Reference/Article_KaiteiNet/WinForm_.txt〕
  *           
  *@content KT 6. Control
- *@subject
+ *@subject ◆Fontクラス System.Windows.Forms
+ *         Font new Font(string familyname, float size, [FontStyle])
+ *             enum FontStyle
+ *             {
+ *                  Regular = 0,  //default 標準
+ *                  Bold = 1,　   //太字
+ *                  Italic = 2,   //斜体
+ *                  Underline = 4,//下線
+ *                  Strikeout = 8,//取消線
+ *             }
+ *             ※列挙体はビットフィールドの形式
+ *             複数指定の場合はビット論理和「|」区切り
+ *             FontStyle.Bold | FontStyle.Strikeout
+ *          
+ *          void font.Dispose()        Fontオブジェクトの破棄
+ *          Font control.DefaultFont   既定のフォント
+ *          Font control.CaptionFont   タイトルのフォント
+ *          Font control.MenuFont      メニューのフォント
+ *          
+ *         ◆SystemFontsクラス システム既定のフォント
+ *                            Dispose()する必要がない。
+ *         ◆SystemColorsクラス システム既定の色
+ *         
+ *@subject ◆Labelクラス System.Windows.Forms
+ *         Label new Label()
+ *         string label.Text
+ *         Point  control.Location
+ *         Color  control.ForeColor
+ *         Color  control.BackColor
+ *         ContentAlignment label.TextAlign    Label領域内での配置位置
+ *                                             Sizeを大きくして配置位置を変更するといい
+ *            enum ContentAlignment
+ *            {
+ *                TopLeft = 1,         // 上端左寄せ
+ *                TopCenter = 2,       // 上端中央
+ *                TopRight = 4,        // 上端右寄せ
+ *                MiddleLeft = 16,     // 中段左寄せ
+ *                MiddleCenter = 32,   // 中段中央
+ *                MiddleRight = 64,    // 中段右寄せ
+ *                BottomLeft = 256,    // 下端左寄せ
+ *                BottomCenter = 512,  // 下端中央
+ *                BottomRight = 1024,  // 下端右寄せ
+ *            }
  *
  *@see FormLabelFont.jpg
  *@author shika
@@ -24,9 +66,9 @@ namespace WinFormGUI.WinFormSample.KaiteiNet.KT06_Control
 {
     class MainLabelFont
     {
-        [STAThread]
-        static void Main()
-        //public void Main()
+        //[STAThread]
+        //static void Main()
+        public void Main()
         {
             Application.EnableVisualStyles();
             Application.Run(new FormLabelFont());
