fecf7de698bab9d726b4fa519002cc20164861fc
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-07-04 21:38:36 +0900

    2022-07-04-2130

---
+++ WinFormSample/KaiteiNet/KT06_Control/MainCheckBoxSample.cs
@@ -0,0 +1,110 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / KaiteiNet / KT06_Control
+ *@class MainCheckBoxSample.cs
+ *@class FontCheckBoxSample.cs
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2010
+ *           http://kaitei.net/csforms/controls/ 
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm06_Control.txt〕
+ *           
+ *@content KT 6. Control / CheckBox
+ *@subject ◆CheckBoxクラス System.Windows.Forms
+ *         CheckBox new CheckBox()
+ *         bool         check.Checked        チェック状態か
+ *         EventHandler check.CheckedChanged チェック状態が変更されたときに発生するイベント
+ *
+ *@subject イベントハンドラー
+ *         ・２つの CheckBoxのイベントハンドラー・メソッドを作るのではなく、
+ *           １つのイベントハンドラーで複合条件分岐する。
+ *         ・Checkedのデフォルト値の設定をイベントハンドラーの後ろに記述する
+ *           (コンストラクタなどで Checkedを先に記述すると、
+ *            チェック状態なのに、チェック時の処理は行われていない状態で描画)
+ *            
+ *         ・三項条件式:   [条件式] ? [true時の値] : [false時の値] 
+ *         
+ *         ・複合条件は ビット論理和演算子「|」で区切る
+ *          FontStyle fontstyle = 0 | FontStyle.Italic; 
+ *          FontStyle fontstyle = FontStyle.Bold | 0;
+ *          FontStyle fontstyle = FontStyle.Bold | FontStyle.Italic;
+ *          
+ *@see FormCheckBoxSample.jpg
+ *@author shika
+ *@date 2022-07-04
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT06_Control
+{
+    class MainCheckBoxSample
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FormCheckBoxSample());
+        }//Main()
+    }//class
+
+    class FormCheckBoxSample : Form
+    {
+        private Font font;
+        private Label label;
+        private CheckBox cbBold;
+        private CheckBox cbItalic;
+
+        public FormCheckBoxSample()
+        {
+            this.Text = "FormCheckBoxSample";
+            font = new Font("Times New Roman", 20);
+
+            label = new Label()
+            {
+                Text = "Times New Roman",
+                Font = font,
+                ForeColor = Color.CornflowerBlue,
+                Location = new Point(20, 70),
+                AutoSize = true,
+            };
+
+            cbBold = new CheckBox()
+            {
+                Text = "Bold (太字)",
+                Location = new Point(20, 15),
+            };
+            cbBold.CheckedChanged += new EventHandler(checkBox_CheckedChanged);
+
+            cbItalic = new CheckBox()
+            {
+                Text = "Italic (斜体)",
+                Location = new Point(20, 40),
+            };
+            cbItalic.CheckedChanged += new EventHandler(checkBox_CheckedChanged);
+
+            cbBold.Checked = true;
+            cbItalic.Checked = true;
+
+            this.Controls.AddRange(new Control[]
+            {
+                cbBold, cbItalic, label,
+            });
+        }//constructor
+
+        private void checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            font.Dispose();
+
+            FontStyle fontStyle =
+                (cbBold.Checked   ? FontStyle.Bold : 0) |
+                (cbItalic.Checked ? FontStyle.Italic : 0);
+
+            font = new Font("Times New Roman", 20, fontStyle);
+
+            label.Font = font;
+        }//check_CheckedChanged()
+    }//class
+}
---
+++ WinFormSample/KaiteiNet/KT06_Control/MainRadioButtonSample.cs
@@ -0,0 +1,101 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / KaiteiNet / KT06_Control
+ *@class MainRadioButtonSample.cs
+ *@class FormRadioButtonSample.cs
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2010
+ *           http://kaitei.net/csforms/controls/ 
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm06_Control.txt〕
+ *           
+ *@content KT 6.Control / RadioButton, GroupBox
+ *@subject ◆RadioButton -- System.Windows.Forms
+ *         ・CheckBoxと異なり，1 グループ内に必ず 1 つのボタンだけが選択された状態を取ります。
+ *         ・そのため，各RadioButtonは，必ずどこかのGroupBoxに所属させます。
+ *         bool         radio.Checked
+ *         EventHandler radio.CheckedChanged
+ *
+ *@subject ◆GroupBox -- System.Windows.Forms
+ *         ・GroupBoxに RadioButton[]を乗せ、Form/Panelには GroupBoxを乗せる
+ *         string group.Text  GroupBoxのタイトル
+ *         
+ *@see FormRadioButtonSample_withGroupBox.jpg
+ *@author shika
+ *@date 2022-7-04
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT06_Control
+{
+    class MainRadioButtonSample
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FormRadioButtonSample());
+        }//Main()
+    }//class
+
+    class FormRadioButtonSample : Form
+    {
+        private Label label;
+        private RadioButton[] radioAry;
+        private GroupBox group;
+        private readonly string[] itemAry = new string[]
+        {
+            "青巻紙", "赤巻紙", "黄巻紙"
+        };
+        
+        public FormRadioButtonSample()
+        {
+            this.Text = "FormRadioButtonSample";
+
+            label = new Label()
+            {
+                Location = new Point(20, 150),
+                AutoSize = true,
+            };
+
+            radioAry = new RadioButton[itemAry.Length];
+            for(int i = 0; i < itemAry.Length; i++)
+            {
+                radioAry[i] = new RadioButton()
+                {
+                    Text = itemAry[i],
+                    Left = 20,
+                    Top = i * 22 + 18,
+                };
+
+                radioAry[i].CheckedChanged +=
+                    new EventHandler(radio_CheckedChanged);
+            }//for
+            radioAry[0].Checked = true;
+
+            group = new GroupBox()
+            {
+                Text = "巻紙",
+                Location = new Point(20, 20),
+                Size = new Size(160, 100),
+            };
+            group.Controls.AddRange(radioAry);
+
+            this.Controls.Add(label);
+            this.Controls.Add(group);
+        }//constructor
+
+        private void radio_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radio = (RadioButton)sender;
+
+            if (radio.Checked)
+            {
+                label.Text = $"Selected: {radio.Text}";
+            }
+        }
+    }//class
+}
