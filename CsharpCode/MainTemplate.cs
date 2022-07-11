6464f291609e12ec73dce9f6a44d130cc708f9df
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-07-12 04:11:25 +0900

    2022-07-12-0400

--- CsharpCode/MainTemplate.cs
+++ CsharpCode/MainTemplate.cs
@@ -6,7 +6,7 @@
  *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
  *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
  *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
- *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2010
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2018
  *           http://kaitei.net/csforms/ 
  *           =>〔~/Reference/Article_KaiteiNet/WinForm_.txt〕
  *           
