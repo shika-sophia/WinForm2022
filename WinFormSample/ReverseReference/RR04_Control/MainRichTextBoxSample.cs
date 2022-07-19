c5b0234d7b8d8955d482f791be75c1a104d87a1b
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-07-19 10:21:16 +0900

    2022-07-19-1000

---
+++ WinFormSample/ReverseReference/RR04_Control/MainRichTextBoxSample.cs
@@ -0,0 +1,137 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / ReverseReference / RR04_Control
+ *@class MainRichTextBoxSample.cs
+ *@class FormRichTextBoxSample.cs
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *           
+ *@content RR[93] RichTextBox -- Undo(), Redo() / p177
+ *@subject ◆RichTextBox : TextBoxBase
+ *         RichTextBox  new RichTextBox()
+ *         void         rich.Undo()          処理を戻す
+ *         void         rich.Redo()          戻した処理を再実行
+ *         bool         rich.CanUndo         Undo()可能な処理が存在するか
+ *         bool         rich.CanRedo         Redo()可能な処理が存在するか
+ *         string       rich.UndoActionName  Undo()で戻す処理内容      (日本語で出る)
+ *         string       rich.RedoActionName  Redo()で再実行する処理内容 (日本語で出る)
+ *
+ *@see FormRichTextBoxSample_withUndoRedo.jpg
+ *@author shika
+ *@date 2022-07-19
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.ReverseReference.RR04_Control
+{
+    class MainRichTextBoxSample
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FormRichTextBoxSample());
+        }//Main()
+    }//class
+
+    class FormRichTextBoxSample : Form
+    {
+        private TableLayoutPanel table;
+        private RichTextBox rich;
+        private Label label;
+        private Button btnUndo;
+        private Button btnRedo;
+
+        public FormRichTextBoxSample()
+        {
+            this.Text = "FormRichTextBoxSample";
+            this.AutoSize = true;
+
+            table = new TableLayoutPanel()
+            {
+                ColumnCount = 3,
+                RowCount = 2,
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+            };
+            table.ColumnStyles.Add(
+                new ColumnStyle(SizeType.Percent, 33.4F));
+            table.ColumnStyles.Add(
+                new ColumnStyle(SizeType.Percent, 33.3F));
+            table.ColumnStyles.Add(
+                new ColumnStyle(SizeType.Percent, 33.3F));
+
+            rich = new RichTextBox()
+            {
+                Size = new Size(400, 300),
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+            };
+            table.Controls.Add(rich, 0, 0);
+            table.SetColumnSpan(rich, 3);
+
+            label = new Label()
+            {
+                TextAlign = ContentAlignment.MiddleCenter,
+                BorderStyle = BorderStyle.Fixed3D,
+                Dock = DockStyle.Fill,
+            };
+            table.Controls.Add(label, 0, 1);
+
+            btnUndo = new Button()
+            {
+                Text = "Undo",
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+            };
+            btnUndo.Click += new EventHandler(btnUndo_Click);
+            table.Controls.Add(btnUndo, 1, 1);
+
+            btnRedo = new Button()
+            {
+                Text = "Redo",
+                Dock = DockStyle.Fill, 
+                AutoSize = true,
+            };
+            btnRedo.Click += new EventHandler(btnRedo_Click);
+            table.Controls.Add(btnRedo, 2, 1);
+
+            this.Controls.Add(table);
+        }//constructor
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            label.Text = rich.UndoActionName;
+
+            DialogResult result = ShowMessage(
+                $"[ {rich.UndoActionName} ]\n処理を戻しますか？",
+                "Undo Confirm");
+            if(result == DialogResult.OK)
+            {
+                rich.Undo();
+            }
+
+            label.Text = "";
+        }
+
+        private void btnRedo_Click(object sender, EventArgs e)
+        {
+            rich.Redo();
+        }
+
+        private DialogResult ShowMessage(string text, string caption)
+        {
+            DialogResult result = MessageBox.Show(
+                text,
+                caption,
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question);
+
+            return result;
+        }//ShowMessage()
+    }//class
+
+}
--- WinFormSample/ReverseReference/RR04_Control/MainTreeViewSample.cs
+++ WinFormSample/ReverseReference/RR04_Control/MainTreeViewSample.cs
@@ -84,11 +84,46 @@
  *         
  *@subject BuildInitialNode() 自己定義メソッド、初期階層の構築
  *         新規 WindowsForm -> TreeView -> [Nodeの編集]ダイアログにて編集
- *         実際の階層と順番が異なるのは、未宣言のノードを子ノードとして配列に代入できないため
+ *         
+ *         【考察】実際の階層と順番が異なるのは、未宣言のノードを子ノードとして配列に代入できないため
  *         子ノードを持たない末端ノードから宣言されるから。
  *
  *         => Form1.Designer.cs / InitializeComponent()内にコード自動生成
- *
+ *         => BuildInitialNode()〔下記 文末〕
+ *         
+ *        【考察】BuildInitialNodeRV()
+ *         new TreeNode(string text, TreeNode[]);
+ *         を用いて全てのNodeを定義しようとするから、上記の問題が起こる。
+ *         定義順が乱雑だと、初期ノードを変更/編集する際に、とても混乱する。
+ *         いわゆる汚いコードになっている。
+ *         Windows Formの GUIで階層構造を作成できるのは便利だが、
+ *         コードビハインドに、これが埋め込まれるのは、
+ *         初期ノードを編集しないことを前提にしている様子。
+ *            ↓
+ *         BuildInitialNodeRV()として ReVersion、階層構造の順で定義してみた。
+ *         tree.Nodes.Add(TreeNode)
+ *         node.Nodes.Add(TreeNode)
+ *         を利用している。
+ *         
+ *         AddRange(TreeNode[])を使用しなかったのは、
+ *         tree.Nodes/node.Nodes というクラス内部に配列が すでに存在するのに、
+ *         2,3つのために新たな配列を newする必要性を感じなかった。
+ *         このことは自動生成の旧メソッドでコンストラクタのために
+ *         大量のnew 配列をしていることにも言える。
+ *         
+ *         Add()の列記ほうが、AddRange(new TreeNode[])より、
+ *         わずかだが、処理も速く、メモリも小さくて済むはず。
+ *         大量のノードを扱う場合は AddRange()のほうがいいかも。
+ *         
+ *         Windows Formの GUIからのコード自動生成は大量ノードを扱い、
+ *         しかも初期ノードを変更しない前提で利用すべき。
+ *         でも大量のノードを扱うなら、ASP.NETのように XMLファイルをロードするか、
+ *         txtファイルをロードするようにしたらいい。
+ *         
+ *         階層構造のデータから、TreeNodeの階層構造を
+ *         組み立てる自動生成アルゴリズムを考えてみるのも面白い。
+ *         少なくとも、Windows Form GUIからの自動生成アルゴリズムは使えない。
+ *         
  *@see FormTreeViewSample_withAdd.jpg
  *@author shika
  *@date 2022-07-18
@@ -134,7 +169,8 @@ namespace WinFormGUI.WinFormSample.ReverseReference.RR04_Control
                 Dock = DockStyle.Fill,
                 AutoSize = true,
             };
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 300));
+            table.ColumnStyles.Add(
+                new ColumnStyle(SizeType.Absolute, 300));
 
             tree = new TreeView()
             {
@@ -144,7 +180,7 @@ namespace WinFormGUI.WinFormSample.ReverseReference.RR04_Control
                 PathSeparator = " / \n",
                 Dock = DockStyle.Fill,
             };
-            BuildInitialNode(); //自己定義メソッド -- 初期ノード階層
+            BuildInitialNodeRV(); //自己定義メソッド -- 初期ノード階層
             table.Controls.Add(tree, 0, 0);
             table.SetRowSpan(tree, 6);
 
@@ -289,37 +325,92 @@ namespace WinFormGUI.WinFormSample.ReverseReference.RR04_Control
             return result;
         }//ShowMessage()
 
-        private void BuildInitialNode()
+        private void BuildInitialNodeRV()
         {
-            TreeNode treeNode1 = new TreeNode("第5011機械工兵中隊");
-            TreeNode treeNode2 = new TreeNode("第501工兵中隊",
-                new TreeNode[] {treeNode1});
-            TreeNode treeNode3 = new TreeNode("第１装甲師団 (グデーリアン)",
-                new TreeNode[] {treeNode2});
-            TreeNode treeNode4 = new TreeNode("第502工兵中隊");
-            TreeNode treeNode5 = new TreeNode("第２装甲師団 (マンシュタイン)",
-                new TreeNode[] {treeNode4});
-            TreeNode treeNode6 = new TreeNode("自動車衛生中隊");
-            TreeNode treeNode7 = new TreeNode("自動車補給中隊");
-            TreeNode treeNode8 = new TreeNode("第51自動車歩兵師団 (リスト)",
-                new TreeNode[] {treeNode6,　treeNode7});
-            TreeNode treeNode9 = new TreeNode("A軍集団 (ルントテンシュタット)",
-                new TreeNode[] {treeNode3, treeNode5, treeNode8});
-            TreeNode treeNode10 = new TreeNode("第10装甲師団 (モーゼル)");
-            TreeNode treeNode11 = new TreeNode("第101自動車歩兵師団 (ハインリヒ)");
-            TreeNode treeNode12 = new TreeNode("B軍集団 (ポック)",
-                new TreeNode[] {treeNode10, treeNode11});
-            TreeNode treeNode13 = new TreeNode("第32軽戦車旅団 (エメリッヒ)");
-            TreeNode treeNode14 = new TreeNode("衛生中隊");
-            TreeNode treeNode15 = new TreeNode("補給中隊");
-            TreeNode treeNode16 = new TreeNode("第171歩兵師団 (メッサーシュミット)",
-                new TreeNode[] {treeNode14, treeNode15});
-            TreeNode treeNode17 = new TreeNode("DAK (ロンメル)", 
-                new TreeNode[] {treeNode13, treeNode16});
+            var root1 = new TreeNode("A軍集団 (ルントテンシュタット)");
+            var root2 = new TreeNode("B軍集団 (ポック)");
+            var root3 = new TreeNode("DAK (ロンメル)");
 
-            tree.Nodes.Add(treeNode9);  //A軍集団
-            tree.Nodes.Add(treeNode12); //B軍集団
-            tree.Nodes.Add(treeNode17); //DAK
-        }//BuildInitialNode()
+            tree.Nodes.Add(root1);
+            tree.Nodes.Add(root2);
+            tree.Nodes.Add(root3);
+
+            //---- root1 ----
+            var parent1 = new TreeNode("第１装甲師団 (グデーリアン)");
+            var child1 = new TreeNode("第501工兵中隊");
+            var ground1 = new TreeNode("第5011機械工兵中隊");
+
+            var parent2 = new TreeNode("第２装甲師団 (マンシュタイン)");
+            var child2 = new TreeNode("第502工兵中隊");
+
+            var parent3 = new TreeNode("第101自動車歩兵師団 (ハインリヒ)");
+            var child3 = new TreeNode("自動車衛生中隊");
+            var child4 = new TreeNode("自動車補給中隊");
+
+            root1.Nodes.Add(parent1);
+            parent1.Nodes.Add(child1);
+            child1.Nodes.Add(ground1);
+
+            root1.Nodes.Add(parent2);
+            parent2.Nodes.Add(child2);
+
+            root1.Nodes.Add(parent3);
+            parent3.Nodes.Add(child3);
+            parent3.Nodes.Add(child4);
+
+            //---- root2 ----
+            var parent4 = new TreeNode("第10装甲師団 (モーゼル)");
+            var parent5 = new TreeNode("第101自動車歩兵師団 (ハインリヒ)");
+
+            root2.Nodes.Add(parent4);
+            root2.Nodes.Add(parent5);
+
+            //---- root3 ----
+            var parent6 = new TreeNode("第32軽戦車旅団 (エメリッヒ)");
+            var parent7 = new TreeNode("第171歩兵師団 (メッサーシュミット)");
+            var child5 = new TreeNode("衛生中隊");
+            var child6 = new TreeNode("補給中隊");
+
+            root3.Nodes.Add(parent6);
+            root3.Nodes.Add(parent7);
+            parent7.Nodes.Add(child5);
+            parent7.Nodes.Add(child6);
+        }//BuildInitialNodeRV()
     }//class
 }
+
+/*
+//====== FormGui -> Auto Created Code ======
+//private void BuildInitialNode()
+//{
+//    TreeNode treeNode1 = new TreeNode("第5011機械工兵中隊");
+//    TreeNode treeNode2 = new TreeNode("第501工兵中隊",
+//        new TreeNode[] {treeNode1});
+//    TreeNode treeNode3 = new TreeNode("第１装甲師団 (グデーリアン)",
+//        new TreeNode[] {treeNode2});
+//    TreeNode treeNode4 = new TreeNode("第502工兵中隊");
+//    TreeNode treeNode5 = new TreeNode("第２装甲師団 (マンシュタイン)",
+//        new TreeNode[] {treeNode4});
+//    TreeNode treeNode6 = new TreeNode("自動車衛生中隊");
+//    TreeNode treeNode7 = new TreeNode("自動車補給中隊");
+//    TreeNode treeNode8 = new TreeNode("第51自動車歩兵師団 (リスト)",
+//        new TreeNode[] {treeNode6,　treeNode7});
+//    TreeNode treeNode9 = new TreeNode("A軍集団 (ルントテンシュタット)",
+//        new TreeNode[] {treeNode3, treeNode5, treeNode8});
+//    TreeNode treeNode10 = new TreeNode("第10装甲師団 (モーゼル)");
+//    TreeNode treeNode11 = new TreeNode("第101自動車歩兵師団 (ハインリヒ)");
+//    TreeNode treeNode12 = new TreeNode("B軍集団 (ポック)",
+//        new TreeNode[] {treeNode10, treeNode11});
+//    TreeNode treeNode13 = new TreeNode("第32軽戦車旅団 (エメリッヒ)");
+//    TreeNode treeNode14 = new TreeNode("衛生中隊");
+//    TreeNode treeNode15 = new TreeNode("補給中隊");
+//    TreeNode treeNode16 = new TreeNode("第171歩兵師団 (メッサーシュミット)",
+//        new TreeNode[] {treeNode14, treeNode15});
+//    TreeNode treeNode17 = new TreeNode("DAK (ロンメル)", 
+//        new TreeNode[] {treeNode13, treeNode16});
+//
+//    tree.Nodes.Add(treeNode9);  //A軍集団
+//    tree.Nodes.Add(treeNode12); //B軍集団
+//    tree.Nodes.Add(treeNode17); //DAK
+//}//BuildInitialNode()
+ */
