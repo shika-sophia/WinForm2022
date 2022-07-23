1481f72737df7d6c9b208647e8e1e4cfdd74ef00
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-07-24 06:11:03 +0900

    2022-07-24-0600

---
+++ WinFormSample/KaiteiNet/KT11_MenuOld/MainStatusBarSample.cs
@@ -0,0 +1,148 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / KaiteiNet / KT11_MenuOld 
+ *@class MainStatusBarSample.cs
+ *@class FormStatusBarSample.cs
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2018
+ *           http://kaitei.net/csforms/menus/
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm11_MenuOld.txt〕
+ *           
+ *@content KT11 MenuOld / StatusBar
+ *         Formの最下段の枠線に テキストを表示するコントロール
+ *
+ *@subject ◆StatusBar : Control
+ *         StatusBar    new StatusBar()
+ *         bool         statusBar.ShowPanels      Panelを表示するか / デフォルト false
+ *         StatusBarPanelCollection  statusBar.Panels 
+ *         int            statusBar.Panels.Add(StatusBarPanel value)
+ *         StatusBarPanel statusBar.Panels.Add(string text);
+ *         void           statusBar.Panels.AddRange(StatusBarPanel[] panels)
+ *                         :  他 IList, ICollection 系メソッド
+ *                         
+ *@subject StatusBarPanel : Component
+ *         StatusBarPanel  new StatusBarPanel()
+ *         string          statusBar.Panel.Text             表示する文字列
+ *         StatusBarPanelAutoSize  statusBarPanel.AutoSize
+ *           └ enum StatusBarPanelAutoSize
+ *             {    
+ *                 None = 1,     //ControlのSizeが変更されても、StatusBarSizeは不変
+ *                 Spring = 2,   //StatusBar 上の使用できる領域のうち None, Contents以外の領域を
+ *                                 Springに指定した他のPanel と共有する (？意味不明)
+ *                 Contents = 3  //StatusBarPanel の幅は、その内容によって決定
+ *             }
+ *             
+ *@subject Event
+ *         EventHandler  menuItem.Click    クリック時のイベント
+ *         EventHandler  menuItem.Select   フォーカスを得たときのイベント
+ *         EventHandler  form.MenuComplete フォーカスが外れたときのイベント
+ *         
+ *@subject Deployment / 配置
+ *         Form / Panel                               Controlとして乗せる
+ *           └ form.Controls.Add(statusBar)           StatusBarインスタンス
+ *               statusBar.Panels.Add(statusBarPanel) SatatusBarに StatusBarPanelを乗せる
+ *         
+ *@see FormStatusBarSample.jpg
+ *@author shika
+ *@date 2022-07-23
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT11_MenuOld
+{
+    class MainStatusBarSample
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FormStatusBarSample());
+        }//Main()
+    }//class
+
+    class FormStatusBarSample : Form
+    {
+        private MainMenu menu;
+        private StatusBar status;
+        private StatusBarPanel statusPanel;
+
+        public FormStatusBarSample()
+        {
+            this.Text = "FormStatusBarSample";
+            this.Font = new Font("consolas", 12, FontStyle.Regular);
+            this.AutoSize = true;
+
+            menu = new MainMenu();
+            //---- Parent ----
+            var menuFile = new MenuItem("File");
+
+            //---- Children ----
+            var menuNew = new MenuItem("New File (&N)");
+            var menuLine = new MenuItem("-");
+            var menuExit = new MenuItem("Close (&X)");
+
+            //---- StatusBar ----
+            status = new StatusBar()
+            {
+                ShowPanels = true,
+            };
+
+            statusPanel = new StatusBarPanel()
+            {
+                AutoSize = StatusBarPanelAutoSize.Spring,
+            };
+
+            status.Panels.Add(statusPanel);
+
+            //---- Event ----
+            menuNew.Click += new EventHandler(menuNew_Click);
+            menuNew.Select += new EventHandler(menuNew_Select);
+            menuExit.Click += new EventHandler(menuExit_Click);
+            menuExit.Select += new EventHandler(menuExit_Select);
+            this.MenuComplete += new EventHandler(form_MenuComplete);
+
+            //---- Deployment ----
+            menu.MenuItems.AddRange(new MenuItem[]
+            {
+                menuFile,
+            });
+
+            menuFile.MenuItems.AddRange(new MenuItem[]
+            {
+                menuNew, menuLine, menuExit,
+            });
+
+            this.Controls.Add(status);
+            this.Menu = menu;
+        }//constructor
+
+        private void menuNew_Click(object sender, EventArgs e)
+        {
+            new FormStatusBarSample().Show();
+        }
+
+        private void menuNew_Select(object sender, EventArgs e)
+        {
+            statusPanel.Text = "It will create new Form.";
+        }
+
+        private void menuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuExit_Select(object sender, EventArgs e)
+        {
+            statusPanel.Text = "It will close this Form.";
+        }
+
+        private void form_MenuComplete(object sender, EventArgs e)
+        {
+            statusPanel.Text = "";
+        }
+    }//class
+}
---
+++ WinFormSample/KaiteiNet/KT11_MenuOld/MainToolBarSample.cs
@@ -0,0 +1,156 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / KaiteiNet / KT11_MenuOld
+ *@class MainToolBarSample.cs
+ *@class FormToolBarSample.cs
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2018
+ *           http://kaitei.net/csforms/menus/
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm11_MenuOld.txt〕
+ *           
+ *@content KT11 MenuOld / ToolBar
+ *         アイコンボタンを並べて表示するControl
+ *         表示位置はクライアント領域内 (TopBarではない)
+ *         
+ *@subject ◆ToolBar : Control
+ *         ToolBar   new ToolBar()
+ *         string    toolBar.Text          ツールバー用の文字列〔？意味不明〕
+ *         bool      toolBar.Divider       区切線を表示するか
+ *         ImageList control.ImageList     ToolBarButtonになるアイコンList
+ *        (bool      control.ShowToolTips  マウスポイント時にヒントを表示するか)
+ *         
+ *         ToolBarAppearance        toolBar.Appearance
+ *           └ enum ToolBarAppearance
+ *             {
+ *                 Normal = 0,  // 3Dボタン
+ *                 Flat = 1     // 平面表示、マウスポイント時に 3D
+ *             }
+ *             
+ *         ToolBarTextAlign        toolBar.TextAlign
+ *           └ enum ToolBarTextAlign   各アイコンのText位置
+ *             {
+ *                 Underneath = 0,  //Textは、ToolBarButtonのイメージの下に配置
+ *                 Right = 1        //Textは、ToolBarButtonのイメージの右に配置
+ *             }
+ *             
+ *         ToolBarButtonCollection  toolBar.Buttons  
+ *         int     toolBar.Buttons.Add(string text);
+ *         int     toolBar.Buttons.Add(ToolBarButton button);
+ *         void    toolBar.Buttons.AddRange(ToolBarButton[] buttons);
+ *         void    toolBar.Buttons.Clear();  
+ *                           :   他 IList, ICollection系 メソッド
+ *                           
+ *         ToolBarButtonClickEventHandler 
+ *                 toolBar.ButtonClick     ToolBarButtonクリック時のイベント
+ *                (ToolBarButtonクラスのイベントではないことに注意)
+ *            ||
+ *         delegate void ToolBarButtonClickEventHandler(
+ *             object sender, ToolBarButtonClickEventArgs e);
+ *             └ ToolBarButtonClickEventArgsクラス
+ *               ToolBarButton   e.Button  // クリックされたToolBarButton
+ *               
+ *@subject ToolBarButton : Component
+ *         ToolBarButton  new ToolBarButton()
+ *         ToolBarButton  new ToolBarButton(string text)
+ *         
+ *         string  toolBarButton.Text        各ボタンの文字列 / デフォルト ""
+ *         int     toolBarButton.ImageIndex  toolBar.ImageListのアイコンを指定する index
+ *         string  toolBarButton.ImageKey    各ボタンのアイコンの名前
+ *        (string  control.ToolTipText   マウスポイント時に表示するヒントの文字列)
+ *        
+ *         ToolBarButtonStyle  toolBarButton.Style
+ *           └ enum ToolBarButtonStyle
+ *             {
+ *                 PushButton = 1,  // 標準の 3D ボタン
+ *                 ToggleButton = 2,// クリックにより凸凹を交互に繰り返す
+ *                 Separator = 3,    // 空白または線。 外観は、toolBar.Appearance値によって異なる
+ *                 DropDownButton = 4 //クリック時に、Menu or 他Formを表示するドロップダウン
+ *             }
+ *        
+ *@subject 相対パス "../../Image/DocumentIcon36px.png"
+ *         「~/bin/Debug/WinFormGUI.exe」からの相対パス
+ *         
+ *@see FormToolBarSample.jpg
+ *@author shika
+ *@date 2022-07-23
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT11_MenuOld
+{
+    class MainToolBarSample
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new FormToolBarSample());
+        }//Main()
+    }//class
+
+    class FormToolBarSample : Form
+    {
+        private ToolBar toolBar;
+        private ToolBarButton toolButtonNew;
+        private ToolBarButton toolButtonExit;
+
+        public FormToolBarSample()
+        {
+            this.Text = "FormToolBarSample";
+            this.Font = new Font("consolas", 12, FontStyle.Regular);
+            this.AutoSize = true;
+
+            var imageList = new ImageList()
+            {
+                ImageSize = new Size(36, 36),
+                TransparentColor = Color.Magenta,
+            };
+            imageList.Images.Add(new Bitmap("../../Image/DocumentIcon36px.png"));
+            imageList.Images.Add(new Bitmap("../../Image/DeleteIcon36px.png"));
+
+            toolButtonNew = new ToolBarButton()
+            {
+                ToolTipText = "New",
+                ImageIndex = 0,
+            };
+
+            toolButtonExit = new ToolBarButton()
+            {
+                ToolTipText = "Exit",
+                ImageIndex = 1,
+            };
+
+            toolBar = new ToolBar()
+            {
+                ShowToolTips = true,
+                ImageList = imageList,
+            };
+            toolBar.ButtonClick += 
+                new ToolBarButtonClickEventHandler(toolBar_ButtonClick);
+            toolBar.Buttons.AddRange(new ToolBarButton[]
+            {
+                toolButtonNew, toolButtonExit,
+            });
+            
+            this.Controls.Add(toolBar);
+        }//constructor
+
+        private void toolBar_ButtonClick(
+            object sender, ToolBarButtonClickEventArgs e)
+        {
+            switch (e.Button.ToolTipText)
+            {
+                case "New":
+                    new FormToolBarSample().Show();
+                    break;
+                case "Exit":
+                    this.Close();
+                    break;
+            }//switch
+        }//toolBar_ButtonClick()
+    }//class
+}
