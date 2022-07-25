4211275b9c3f20976f59ed0c1ec40f4f64f9edff
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-07-26 07:14:44 +0900

    2022-07-26-0700

---
+++ WinFormSample/KaiteiNet/KT03_Form/MainFormColorSample.cs
@@ -0,0 +1,44 @@
+﻿/*
+ *@title WinFormSample / KaiteiNet / KT03_Form / MainFormColorSample.cs
+ *@see   MainFormConstruct.cs
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.KaiteiNet.KT03_Form
+{
+    class MainFormColorSample
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Application.Run(new FormColorSample());
+        }//Main()
+    }//class
+
+    class FormColorSample : Form
+    {
+        public FormColorSample()
+        {
+            this.Text = "FormColorSample";
+            this.ForeColor = Color.White;
+            this.BackColor = Color.Pink;
+
+            Label label = new Label();
+            label.Text = "文字色";            
+            this.Controls.Add(label);
+            
+            this.BackgroundImage = Image.FromFile("../../Image/Icon/squreTile40px.png");
+            this.BackgroundImageLayout = ImageLayout.Tile;
+        }
+    }//class
+}
+
+/*
+//これだとエラーが出る
+//Bitmap bitmap = new Bitmap("../../Image/squreTile40px.png");
+//this.BackgroundImage = bitmap;
+//bitmap.Dispose();
+*/
