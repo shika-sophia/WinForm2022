515d98cebccdba3f3ce81ab9223806450df4102c
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-08-29 15:38:47 +0900

    2022-08-29-1530

---
+++ WinFormSample/ReverseReference/RR08_Graphics/MainColorMatrixSemiTransParent.cs
@@ -0,0 +1,185 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / ReverseReference / RR08_Graphics
+ *@class MainColorMatrixSemiTransParent.cs
+ *@class   └ new FormColorMatrixSemiTransParent() : Form
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2018
+ *           http://kaitei.net/csforms/graphics/
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm07_Graphics.txt〕
+ *           
+ *@content RR[371] p625 ColorMatrix SemiTransParent
+ *         画像を半透明にして描画
+ *         semitransparent [英]: 半透明な
+ *         translucent  [英]: 半透明な, 透明感
+ *         translucency [英]: 透明度, 透明性, 透過性
+ *         matrix [英]: 行列
+ *         
+ *@subject Graphics
+ *         void  graphics.DrawImage(
+ *                 Image, Rectangle destRect, Rectangle srcRect, GraphicsUnit)
+ *                   └ 引数 destRect: 描画する位置とサイズを四角形で指定
+ *                     引数 srcRect:  描画するImageの部分画像の範囲を四角形で指定
+ *                 ※ RectangleFも可、int, float, Point[], PointF[]に置換するオーバーロードあり
+ *         
+ *         void  graphics.DrawImage(
+ *                 Image, PointF[] destPoints, RectangleF srcRect,
+ *                 GraphicsUnit, ImageAttributes)
+ *                 ※ Point[]不可, Rectangle不可
+ *                 
+ *         void  graphics.DrawImage(
+ *                 Image, Rectangle, int x, int y, int width, int height,
+ *                 GraphicsUnit, ImageAttributes)
+ *                   └ enum GraphicsUnits =>〔KT07_Graphics/MainDrawImageSample.cs〕
+ *                 ※ RectangleF不可, floatも可
+ *                 
+ *@subject ImageAttributes : ICloneable, IDisposable
+ *            -- System.Drawing.Imaging.
+ *         ImageAttributes   new ImageAttributes()
+ *         
+ *           :
+ *           
+ *
+ *@subject ◆ColorMatrix class -- System.Drawing.Imaging.
+ *         ・RGBAW空間の 5 × 5 行列を表すクラス
+ *         ・R: Red / G: Green / B: Blue / A: Alpha 透明度 / W: ?? を表す
+ *         ・ImageAttributesクラスの コンストラクタの引数やプロパティの値として利用
+ *         
+ *         ＊透明度の変更 Matrix33 = XXf;  
+ *           XX は [0 ～ 1]の float値   0f:透明, 0.xf: 半透明, 1f:元画像の透明度
+ *           
+ *         ＊色調の変更 RR[372] p626
+ *           変更後の R = r * Matrix00 + g * Matrix01 + b * Matrix02
+ *           変更後の G = r * Matrix10 + g * Matrix11 + b * Matrix12
+ *           変更後の B = r * Matrix20 + g * Matrix21 + b * Matrix22
+ *         
+ *         ColorMatrix   new ColorMatrix();
+ *         ColorMatrix   new ColorMatrix(float[][] newColorMatrix)
+ *         
+ *         float colorMatrix[int row, int column]
+ *         float this[int row, int column]
+ *         float colorMatrix.MatrixXX       XX: row-column, 0-4
+ *         例: colorNatrix.Matrix33 = 0.5f; とすると半透明の画像になる
+ *         
+ *@see ImageColorMatrixSemiTransParent.jpg
+ *@see 
+ *@author shika
+ *@date 2022-08-29
+ */
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.ReverseReference.RR08_Graphics
+{
+    class MainColorMatrixSemiTransParent
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Console.WriteLine("new FormColorMatrixSemiTransParent()");
+
+            Application.EnableVisualStyles();
+            Application.Run(new FormColorMatrixSemiTransParent());
+
+            Console.WriteLine("Close()");
+        }//Main()
+    }//class
+
+    class FormColorMatrixSemiTransParent : Form
+    {
+        private readonly TableLayoutPanel table;
+        private readonly PictureBox pic;
+        private readonly Button btnOrigin;
+        private readonly Button btnSemi;
+        private readonly Graphics g;
+        private Image image;
+        private Rectangle rect;
+
+        public FormColorMatrixSemiTransParent()
+        {
+            this.Text = "FormColorMatrixSemiTransParent";
+            this.Font = new Font("consolas", 12, FontStyle.Regular);
+            this.Size = new Size(400, 300);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.BackColor = SystemColors.Window;
+
+            table = new TableLayoutPanel()
+            {
+                ColumnCount = 2,
+                RowCount = 2,
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+            };
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 85f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 15f));
+
+            image = new Bitmap("../../Image/SF101.JPG");
+
+            pic = new PictureBox()
+            {
+                ClientSize = image.Size,
+                Dock = DockStyle.Fill,
+                BorderStyle = BorderStyle.Fixed3D,
+            };
+            g = pic.CreateGraphics();
+            rect = new Rectangle(pic.Location, pic.Size);
+            table.Controls.Add(pic, 0, 0);
+            table.SetColumnSpan(pic, 2);
+
+            btnOrigin = new Button()
+            {
+                Text = "Original",
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+            };
+            btnOrigin.Click += new EventHandler(btn_Click);
+            table.Controls.Add(btnOrigin, 0, 1);
+
+            btnSemi = new Button()
+            {
+                Text = "Semitransparent",
+                Dock = DockStyle.Fill,
+                AutoSize = true,
+            };
+            btnSemi.Click += new EventHandler(btn_Click);
+            table.Controls.Add(btnSemi, 1, 1);
+
+            this.Controls.Add(table);
+        }//constructor
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            g.Clear(SystemColors.Window);
+            string btnText = (sender as Button).Text;
+
+            var cm = new ColorMatrix();
+            cm.Matrix00 = 1f;
+            cm.Matrix11 = 1f;
+            cm.Matrix22 = 1f;
+            //cm.Matrix33 = xx;  -> switch〔below〕
+            cm.Matrix44 = 1f;
+
+            switch (btnText)
+            {
+                case "Original":
+                    cm.Matrix33 = 1f;
+                    break;
+                case "Semitransparent":
+                    cm.Matrix33 = 0.5f;
+                    break;
+            }//switch
+
+            var imageAttr = new ImageAttributes();
+            imageAttr.SetColorMatrix(cm);
+
+            g.DrawImage(image, rect, 0, 0, image.Width, image.Height,
+                GraphicsUnit.Pixel, imageAttr);
+        }//btn_Click()
+    }//class
+}
--- WinFormSample/ReverseReference/RR08_Graphics/MainPathGradientBrushSample.cs
+++ WinFormSample/ReverseReference/RR08_Graphics/MainPathGradientBrushSample.cs
@@ -10,12 +10,67 @@
  *           =>〔~/Reference/Article_KaiteiNet/WinForm07_Graphics.txt〕
  *           
  *@content RR[370] p624 / PathGradientBrush
- *@subject GraphicsPath -- 
- *@subject LinearGradientBrush
- *@subject PathGradientBrush
+ *@subject GraphicsPath =>〔KT07_Graphics\MainGraphicsPathSample.cs〕
+ *         void  graphics.Clear(Color)
+ *                    └ 引数 Color  SystemColors.Window
+ *                Graphicsオブジェクトに以前に描画したものは残り続けるので、
+ *                画面を切り替える場合は 別の Graphicsオブジェクトに描画するか、
+ *                同一Graphicsなら Clear()して使う
+ *                
+ *@subject LinearGradientBrush =>〔KT07_Graphics\MainBrushAppendix.cs〕
+ *@subject ◆PathGradientBrush : Brush -- System.Drawing.Drawing2D
+ *         ・コンストラクタで 塗りつぶし範囲を指定
+ *         ・色情報は コンストラクタに入っていないので、
+ *           CenterColor, SurroundColorsを追加で指定する必要がある
+ *           (コンストラクタで生成したオブジェクトのままだと何も描画されない)
+ *           
+ *         PathGradientBrush     new PathGradientBrush(GraphicsPath, [WrapMode])
+ *         PathGradientBrush     new PathGradientBrush(Point[], [WrapMode])  ※Point[]も可
+ *           └ enum WrapMode  -- System.Drawing.Drawing2D.  塗りつぶし範囲より Brush範囲が小さい場合の並べ方
+ *             {
+ *                 Tile = 0,        //グラデーションまたはテクスチャを並べて表示
+ *                 TileFlipX = 1,   //水平方向に反転し、並べて表示
+ *                 TileFlipY = 2,   //垂直方向に反転し、並べて表示
+ *                 TileFlipXY = 3,  //水平および垂直方向に反転し、並べて表示
+ *                 Clamp = 4        //並べて表示されません
+ *             }
+ *             
+ *         Color       pathGradientBrush.CenterColor { get; set; }    塗りつぶし範囲の中央の色  
+ *         Color[]     pathGradientBrush.SurroundColors { get; set; } 塗りつぶし範囲の path周辺の色
+ *         RectangleF  pathGradientBrush.Rectangle { get; }           塗りつぶしpathに外接する四角形
+ *         PointF      pathGradientBrush.CenterPoint { get; set; }    グラデーションの中央点
+ *         WrapMode    pathGradientBrush.WrapMode { get; set; }       塗りつぶし範囲より Brush範囲が小さい場合の並べ方
  *
+ *@NOTE【考察】PictureBox.Size と Dock, SizeMode
+ *      ・BorderStyle, Imageは Dock, SizeModeで拡大されるが 
+ *        Graphicsの描画は Dock, SizeModeで拡大しない
+ *      ・DockStyle.Fill, SizeMode = PictureBoxSizeMode.Zoomなどは
+ *        pictureBox.Imageで Imageオブジェクトを登録した場合のみ有効
+ *      ・pictrureBox.CreateGraphics()に g.DrawXxxx()/g.FillXxxx()した
+ *        グラフィックスには Dock, SizeModeの値が影響しない
+ *        (サイズが小さいまま表示される)
+ *      ・Graphicsを PictureBoxの枠線いっぱいに表示するには
+ *        pictureBox.Size / ClientSizeを 正しく指定する必要がある
+ *        (pic.Size / pic.ClientSizeは DockStyle.Fill, SizeModeで拡大する前の大きさに設定されている)
+ *      ・Size / ClientSizeを定数指定することは望ましくないので、
+ *        PictureBoxの親コントロールである Form / Panelのサイズから取得すべき
+ *      ・Panelである TableLayoutPanelも同様に Dock, ColumnStyles, RowStylesで
+ *        拡大・縮小する前のサイズが Size, Width, Heightの値になっている
+ *        
+ *       ＊例
+ *      ・form.ClientSize.Width - 5: 親Formの横幅から、PictureBoxの枠線分の 5pxを除いた横幅
+ *      ・(int)(form.ClientSize.Height * 0.83): 親Formの高さの 83%の高さ
+ *        intキャストは pic.Sizeが int指定なので要キャスト
+ *        intキャストなのに、* 0.85 -> * 0.83などの変更も ちゃんとサイズに反映する
+ *        (本来の intキャストだと、どちらの値も 0になるはずだが、
+ *         元の 0.85などの値がカプセル化されて、型のみ intになっている様子)
+ *      ・間に Bitmapオブジェクトを入れて
+ *        pic.Image = new Bitmap(pic.Width, pic.Height); として、
+ *        pic.CreateGraphics().DrawXxxx()で描いても、Dock, SizeModeは適用されない
+ *        
  *@see ImagePathGradientBrushSample.jpg
- *@see 
+ *@copyTo ~/WinFormSample/GraphicsRefernce.txt
+ *@copyTo ~/WinFormSample/ColorRefernce.txt
  *@author shika
  *@date 2022-08-28
  */
@@ -28,9 +83,9 @@ namespace WinFormGUI.WinFormSample.ReverseReference.RR08_Graphics
 {
     class MainPathGradientBrushSample
     {
-        [STAThread]
-        static void Main()
-        //public void Main()
+        //[STAThread]
+        //static void Main()
+        public void Main()
         {
             Console.WriteLine("new FormPathGradientBrushSample()");
 
@@ -49,7 +104,6 @@ namespace WinFormGUI.WinFormSample.ReverseReference.RR08_Graphics
         private readonly Button btnPath;
         private readonly Graphics graph;
         private readonly Rectangle rect;
-        private readonly Bitmap bitmap;
 
         public FormPathGradientBrushSample()
         {
@@ -72,17 +126,15 @@ namespace WinFormGUI.WinFormSample.ReverseReference.RR08_Graphics
 
             pic = new PictureBox()
             {
-                Image = bitmap,
                 Size = new Size(
                     this.ClientSize.Width - 5, 
                     (int)(this.ClientSize.Height * 0.83)),
-                SizeMode = PictureBoxSizeMode.StretchImage,
                 Dock = DockStyle.Fill,
                 BorderStyle = BorderStyle.Fixed3D,
             };
             graph = pic.CreateGraphics();
             rect = new Rectangle(pic.Location, pic.ClientSize);
-            bitmap = new Bitmap(pic.Width, pic.Height);
+            
             table.Controls.Add(pic, 0, 0);
             table.SetColumnSpan(pic, 2);
 
