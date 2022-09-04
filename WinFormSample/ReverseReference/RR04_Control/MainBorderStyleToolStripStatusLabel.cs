476a0b4476dd1ad1ca7cdaf40a7d19b371fc3505
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-09-04 14:08:56 +0900

    2022-09-04-1400

---
+++ WinFormSample/ReverseReference/RR04_Control/MainBorderStyleToolStripStatusLabel.cs
@@ -0,0 +1,128 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / ReverseReference.RR04_Control
+ *@based MainBorderStyleViewer.cs
+ *@class MainBorderStyleToolStripStatusLabel.cs
+ *@class   └ new FormBorderStyleToolStripStatusLabel() : Form
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2018
+ *           http://kaitei.net/csforms/menu-strips/
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm12_MenuToolStrip.txt〕
+ *           
+ *@content ToolStripStatusLabel.BorderStyle  〔WF41〕
+ *@subject StatusStrip
+ *         =>〔KT12_MenuToolStrip\MainStatusStripSample.cs〕
+ *         
+ *         Border3DStyle          toolStripStatusLabel.BorderStyle
+ *           └ enum  Border3DStyle
+ *             {
+ *                 RaisedOuter = 1, //外縁だけ凸表示、内縁は非表示
+ *                 SunkenOuter = 2, //外縁だけ凹表示、内縁は非表示
+ *                 RaisedInner = 4, //内縁だけが凸表示され、外縁は非表示
+ *                 Raised = 5,      //内縁と外縁が、凸表示
+ *                 Etched = 6,      //内縁と外縁が、凹表示
+ *                 SunkenInner = 8, //凹表示され、外縁は非表示
+ *                 Bump = 9,        //内縁と外縁が、凸表示
+ *                 Sunken = 10,     //内縁と外縁が凹表示
+ *                 Adjust = 8192,   //境界線が指定した四角形の外側に描画され、四角形の大きさは保持
+ *                 Flat = 16394     //平面表示
+ *             }
+ *
+ *@see ImageBorderStyleToolStripStatusLabel.jpg
+ *@see MainBorderStyleViewer.cs
+ *@see KT12_MenuToolStrip\MainStatusStripSample.cs
+ *@author shika
+ *@date 2022-09-04
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.ReverseReference.RR04_Control
+{
+    class MainBorderStyleToolStripStatusLabel
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Console.WriteLine("new FormBorderStyleToolStripStatusLabel()");
+
+            Application.EnableVisualStyles();
+            Application.Run(new FormBorderStyleToolStripStatusLabel());
+
+            Console.WriteLine("Close()");
+        }//Main()
+    }//class
+
+    class FormBorderStyleToolStripStatusLabel : Form
+    {
+        private readonly TableLayoutPanel table;
+        private readonly Button[] buttonAry;
+        private readonly StatusStrip status;
+        private readonly ToolStripStatusLabel stLabel;
+
+        public FormBorderStyleToolStripStatusLabel()
+        {
+            this.Text = "FormBorderStyleToolStripStatusLabel";
+            this.Font = new Font("consolas", 12, FontStyle.Regular);
+            this.Size = new Size(480, 320);
+            this.BackColor = SystemColors.Window;
+
+            table = new TableLayoutPanel()
+            {
+                ColumnCount = 2,
+                RowCount = 5,
+                Dock = DockStyle.Fill,
+            };
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+
+            status = new StatusStrip();
+            stLabel = new ToolStripStatusLabel()
+            {
+                Text = "RaisedOuter",
+                Spring = true,
+                BorderStyle = Border3DStyle.RaisedOuter,
+                BorderSides = ToolStripStatusLabelBorderSides.All,
+            };
+            status.Items.Add(stLabel);
+
+            Array borderStyleAry = Enum.GetValues(typeof(Border3DStyle));
+            buttonAry = new Button[borderStyleAry.Length];
+            int index = 0;
+            foreach(object value in borderStyleAry)
+            {
+                string name = value.ToString();
+
+                buttonAry[index] = new Button()
+                {
+                    Text = name,
+                    Dock = DockStyle.Fill,
+                    AutoSize = true,
+                    UseVisualStyleBackColor = true,
+                };
+                buttonAry[index].Click += new EventHandler((sender, e) =>
+                {
+                    stLabel.Text = name;
+                    stLabel.BorderStyle = (Border3DStyle)value;
+                    status.Refresh();
+                });
+                table.Controls.Add(buttonAry[index]);
+
+                index++;
+            }//foreach
+
+            this.Controls.AddRange(new Control[]
+            {
+                table, status,
+            });
+        }//constructor
+    }//class
+}
---
+++ WinFormSample/ReverseReference/RR04_Control/MainBorderStyleViewer.cs
@@ -0,0 +1,134 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / ReverseReference / RR04_Control
+ *@class MainBorderStyleViewer.cs
+ *@class   └ new FormBorderStyleViewer() : Form
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *           
+ *@content RR[38] p79 FormBorderStyle
+ *@subject Form -- System.Windows.Forms
+ *         FormBorderStyle  form.FormBorderStyle { get; set; }
+ *                              Formのサイズ変更の可否、境界線のスタイル
+ *                              FormBorderStyle.Xxxxx で指定
+ *           └ enum FormBorderStyle  -- System.Windows.Forms
+ *             { 
+ *                None = 0,             // 境界線なし
+ *                FixedSingle = 1,      // サイズ固定，一重線
+ *                Fixed3D = 2,          // サイズ固定，3D スタイル
+ *                FixedDialog = 3,      // サイズ固定，ダイアログ用
+ *                Sizable = 4,          // サイズ可変 (既定)
+ *                FixedToolWindow = 5,  // サイズ固定，ツールウィンドウ用
+ *                SizableToolWindow = 6 // サイズ可変，ツールウィンドウ用
+ *             }
+ *             
+ *@subject コントロール共通 BorderStyle
+ *         BorderStyle  control.BorderStyle { get; set; }
+ *           └ enum BorderStyle  -- System.Windows.Forms
+ *             {
+ *                None = 0,             // 境界線なし
+ *                FixedSingle = 1,      // サイズ固定，一重線
+ *                Fixed3D = 2,          // サイズ固定，3D スタイル
+ *             }
+ *             
+ *@NOTE【Problem】
+ *      ・FormBorderStyleを変更しても、Formの境界スタイルは、たいして変わらない
+ *      ・サイズ変更の可否は、ちゃんと反映する
+ *      ・Noneにすると、タイトルバーも消える
+ *      ・XxxxToolWindowにすると、タイトルバーの最小化/最大化ボタンが消える
+ *      ・Labelの BorderStyle.Fixed3Dにすると、 左辺と下辺は うまく描画されない
+ *      ・Border3DStyle  ToolStripStatusLabel.BorderStyle は種類が充実している
+ *      
+ *@see ImageBorderStyleViewer.jpg
+ *@see MainBorderStyleToolStripStatusLabel.cs
+ *@author shika
+ *@date 2022-09-04
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.ReverseReference.RR04_Control
+{
+    class MainBorderStyleViewer
+    {
+        //[STAThread]
+        //static void Main()
+        public void Main()
+        {
+            Console.WriteLine("new FormBorderStyleViewer()");
+
+            Application.EnableVisualStyles();
+            Application.Run(new FormBorderStyleViewer());
+
+            Console.WriteLine("Close()");
+        }//Main()
+    }//class
+
+    class FormBorderStyleViewer : Form
+    {
+        private readonly TableLayoutPanel table;
+        private readonly Button[] buttonAry;
+        private readonly Label label;
+
+        public FormBorderStyleViewer()
+        {
+            this.Text = "FormBorderStyleViewer";
+            this.Font = new Font("consolas", 12, FontStyle.Regular);
+            this.Size = new Size(480, 320);
+            this.BackColor = SystemColors.Window;
+
+            table = new TableLayoutPanel()
+            {
+                ColumnCount = 2,
+                RowCount = 5,
+                Dock = DockStyle.Fill,
+            };
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+            table.RowStyles.Add(new RowStyle(SizeType.Percent, 20f));
+
+            Array borderStyleAry = Enum.GetValues(typeof(FormBorderStyle));
+            buttonAry = new Button[borderStyleAry.Length];
+
+            foreach (object value in borderStyleAry)
+            {
+                string name = value.ToString();
+                int index = (int)value;
+
+                buttonAry[index] = new Button()
+                {
+                    Text = name,
+                    Dock = DockStyle.Fill,
+                    AutoSize = true,
+                    UseVisualStyleBackColor = true,
+                };
+                buttonAry[index].Click += new EventHandler((sender, e) =>
+                {
+                    label.Text = name;
+                    this.FormBorderStyle = (FormBorderStyle)value;
+                    this.Refresh();
+                });
+                table.Controls.Add(buttonAry[index]);
+            }//foreach
+
+            label = new Label()
+            {
+                Text = "Sizable",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Dock = DockStyle.Fill,
+                Margin = new Padding(5),
+                BorderStyle = BorderStyle.FixedSingle,
+            };
+            table.Controls.Add(label, 0, 4);
+            table.SetColumnSpan(label, 2);
+
+            this.Controls.Add(table);
+        }//constructor
+
+    }//class
+}
