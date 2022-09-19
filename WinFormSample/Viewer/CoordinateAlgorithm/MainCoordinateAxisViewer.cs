aae1d2e6ffe60f24b46f546c66835ef8b22dddb6
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-09-19 20:05:57 +0900

    2022-09-19-2000

--- WinFormSample/Viewer/CoordinateAlgorithm/MainCoordinateAxisViewer.cs
+++ WinFormSample/Viewer/CoordinateAlgorithm/MainCoordinateAxisViewer.cs
@@ -36,9 +36,9 @@ namespace WinFormGUI.WinFormSample.Viewer.CoordinateAlgorithm
 {
     class MainCoordinateAxisViewer
     {
-        [STAThread]
-        static void Main()
-        //public void Main()
+        //[STAThread]
+        //static void Main()
+        public void Main()
         {
             Console.WriteLine("new FormCoordinateAxisViewer()");
 
