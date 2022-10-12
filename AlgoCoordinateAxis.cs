0f5bddf97e4f85e5888eab3bee8c012fafa74e3a
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-10-12 13:16:06 +0900

    2022-10-12-1320

---
+++ AlgoCoordinateAxis.cs
@@ -0,0 +1,275 @@
+﻿/** 
+ *@title WinFormGUI / WinFormSample / Viewer / CoordinateAlgorithm
+ *@class CoordinateAxis(PictureBox) 
+ *@reference CS 山田祥寛『独習 C＃ [新版] 』 翔泳社, 2017
+ *@reference NT 山田祥寛『独習 ASP.NET [第６版] 』 翔泳社, 2019
+ *@reference RR 増田智明・国本温子『Visual C＃2019 逆引き大全 500の極意』 秀和システム, 2019
+ *@reference KT ナガノ  『Windows Form C#』KaiteiNet, 2018
+ *           http://kaitei.net/csforms/
+ *           =>〔~/Reference/Article_KaiteiNet/WinForm_.txt〕
+ *           
+ *@content CoordinateAxisViewer
+ *         座標軸のみを描画
+ */
+#region Method Algorithm
+/*
+ *@subject - Graphics  BuildGraphics()
+ *         AlgoCoordinateAxis 十字座標系に特化したメソッド
+ *         
+ *         Matrix 平行移動, 拡大縮小
+ *         void  matrix.Translate(float offsetX, float offsetY) 
+ *         void  matrix.Scale(float sx, float sy)  拡大縮小
+ *         g.Transform = matrix;
+ *         を用いて、Graphicsの原点(0, 0)を平行移動し、PictureBoxの中心点と 一致させる。
+ *         
+ *         ・これにより、数学の点の座標のまま、Graphicsの座標にすることができる。
+ *         ・Y座標の反転を image.RotateFlip()で行うと、文字まで反転してしまうので、
+ *           DrawLine(), DrawString()時に Y座標に「-」を付けて反転させる。
+ *
+ *@subject + void  DrawCoordinateAxis()  座標軸の描画
+ *
+ *@subject - void  DrawAxisScale()  軸の目盛り AxisScale 自己定義メソッド
+ *         100, -100ごとに目盛りを付加 (座標範囲によって 1000, -1000)
+ *         
+ *@subject + void DrawMultiPointLine(PointF[], bool autoScale = true)
+ *         複数 PointFの描画
+ *         
+ *         絶対値で最大値(X座標とY座標の比を考慮した最大値)を持つ PointFで
+ *          PointAutoScale()してから DrawPointLine()
+ *          
+ *@subject + void  DrawPointLine(PointF pt, bool withLine = false)
+ *         点の描画
+ *         
+ *@subject + void  PointAutoScale(PointF)
+ *         自動スケール調整
+ *         
+ *         ・引数 PointFが 座標範囲外の場合、座標範囲内になるまで scaleRateを調整
+ *         ・scaleRate = 2.0M, 1.0M, 0.5M, 2.5M, 1.25M ...
+ *         
+ *         while (((decimal)centerPoint.X / scaleRate) < (decimal)pt.X
+ *              || ((decimal)centerPoint.Y / scaleRate) < (decimal)pt.Y)
+ *          {
+ *              SetScaleRate(scaleRate / 2M);
+ *          }//while
+ *
+ *@subject + void  SetScaleRate(decimal)
+ *         decimal  scaleRate プロパティ
+ *         
+ *         各 X, Y座標に scaleRateを乗算する。
+ *         これにより、座標範囲を超えた点も、描画可能。
+ *         座標の目盛りのほうを 50%, 25%, 12.5%...と縮小していく
+ *         縮小していくと目盛りが細かくなりすぎて、見ずらくなるので、
+ *         1000, -1000単位で区切り、1k, 2k, ...と表示
+ *         
+ *         ※ 軸と目盛りはデフォルトで再描画するが、DrawPointLine()以前の
+ *         DrawLine(), DrawString()は消えてしまうので、手動で再描画することに注意
+ */
+#endregion
+/*
+ *@see ImageCoordinateAxisViewer.jpg
+ *@see 
+ *@author shika
+ *@date 2022-09-16
+ */
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WinFormGUI.WinFormSample.Viewer.CoordinateAlgorithm
+{
+    class AlgoCoordinateAxis : AbsAlgoCoordinate
+    {
+        public AlgoCoordinateAxis(PictureBox pic) : base(pic)
+        {
+            g = BuildGraphics();
+        }
+
+        private Graphics BuildGraphics()
+        {
+            Bitmap bitmap = new Bitmap(
+                pic.ClientSize.Width,
+                pic.ClientSize.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.SmoothingMode = SmoothingMode.HighQuality;
+
+            Matrix mx = new Matrix();
+            mx.Translate(centerPoint.X, centerPoint.Y);
+            mx.Scale(0.96f, 0.96f);  //fixed
+            g.Transform = mx;
+            pic.Image = bitmap;
+
+            defaultGrapics = g.Save();
+            return g;
+        }//BuildGraphics()
+
+        //====== Axis ======
+        public void DrawCoordinateAxis()
+        {
+            penBlue.DashStyle = DashStyle.Solid;
+            penBlue.SetLineCap(LineCap.ArrowAnchor, LineCap.ArrowAnchor, DashCap.Flat);
+            g.DrawLine(penBlue, 0, -centerPoint.Y, 0, centerPoint.Y);  // X軸
+            g.DrawLine(penBlue, -centerPoint.X, 0, centerPoint.X, 0);  // Y軸
+
+            Brush brushBlue = penBlue.Brush;
+            g.DrawString("Ｏ", font, brushBlue, -25, 8);
+            g.DrawString("Ｘ", font, brushBlue, (float)((decimal)centerPoint.X - 18M), 8);
+            g.DrawString("Ｙ", font, brushBlue, -25, -centerPoint.Y);
+
+            DrawAxisScale(brushBlue);
+            brushBlue.Dispose();
+        }//DrawCoordinateAxis
+
+        private void DrawAxisScale(Brush brushBlue)
+        {
+            penBlue.DashStyle = DashStyle.Solid;
+            penBlue.SetLineCap(LineCap.Flat, LineCap.Flat, DashCap.Flat);
+            int scaleWidth = 4;
+            SizeF plusSize = g.MeasureString("200", fontSmall);
+            SizeF minusSize = g.MeasureString("-200", fontSmall);
+
+            for (int i = 0;
+                i < (int)centerPoint.X / scaleRate;
+                i += (scaleRate > 0.25M ? 100 : 1000))
+            {
+                if (i == 0) { continue; }
+                g.DrawLine(penBlue, 
+                    (float)(i * scaleRate), scaleWidth,
+                    (float)(i * scaleRate), -scaleWidth);     // X-Axis +
+                g.DrawLine(penBlue, 
+                    (float)(-i * scaleRate), scaleWidth,
+                    (float)(-i * scaleRate), -scaleWidth);   // X-Axis -
+
+                g.DrawString(scaleRate > 0.1M ? $"{i}" : $"{i / 1000}k",
+                    fontSmall, brushBlue,
+                    (float)((i * scaleRate) - (decimal)plusSize.Width / 2M), 10f);
+                g.DrawString(scaleRate > 0.1M ? $"{i}" : $"{i / 1000}k",
+                    fontSmall, brushBlue,
+                    (float)((-i * scaleRate) - (decimal)minusSize.Width / 2M), 10f);
+            }//for
+
+            for (int i = 0;
+                i < (int)centerPoint.Y / scaleRate;
+                i += (scaleRate > 0.25M ? 100 : 1000))
+            {
+                if (i == 0) { continue; }
+                g.DrawLine(penBlue,
+                    -scaleWidth, (float)(-i * scaleRate),
+                    scaleWidth, (float)(-i * scaleRate));   // Y-Axis +
+                g.DrawLine(penBlue, 
+                    -scaleWidth, (float)(i * scaleRate), 
+                    scaleWidth, (float)(i * scaleRate));    // Y-Axis -
+                
+                g.DrawString(scaleRate > 0.05M ? $"{i}" : $"{i / 1000}k",
+                    fontSmall, brushBlue,
+                    -plusSize.Width - 5f, 
+                    (float)((decimal)-i * scaleRate - (decimal)plusSize.Height / 2M));
+                g.DrawString(scaleRate > 0.05M ? $"{i}" : $"{i / 1000}k",
+                    fontSmall, brushBlue,
+                    -minusSize.Width - 5f,
+                    (float)((decimal)i * scaleRate - (decimal)minusSize.Height / 2M));
+            }//for
+        }//DrawAxisScale()
+
+        //====== Draw Point ======
+        public void DrawMultiPointLine(PointF[] pointAry, bool autoScale = true)
+        {
+            //絶対値で最大値(X座標とY座標の比を考慮した最大値)を持つ PointFで
+            //PointAutoScale()してから DrawPointLine()
+
+            decimal max = 0M;
+            PointF maxPoint = new PointF(0, 0);
+            foreach (PointF pt in pointAry)
+            {
+                if (float.IsNaN(pt.X) || float.IsNaN(pt.Y)) { continue; }
+
+                decimal lager = Math.Max(
+                    Math.Abs((decimal)pt.X * ratioWidthHeight),
+                    Math.Abs((decimal)pt.Y));
+
+                if (max < lager)
+                {
+                    max = lager;
+                    maxPoint = pt;
+                }
+            }//foreach
+
+            PointAutoScale(maxPoint, autoScale);
+
+            foreach (PointF pt in pointAry)
+            {
+                DrawPointLine(pt);
+            }//forech
+        }//DrawMultiPointLine()
+
+        public void DrawPointLine(PointF pt, bool withLine = false)
+        {
+            //PointAutoScale(pt);
+
+            if (float.IsNaN(pt.X) || float.IsNaN(pt.Y)) { return; }
+
+            Brush brushPink = penPink.Brush;
+            g.FillEllipse(brushPink,
+                (float)((decimal)pt.X * scaleRate - 3M),
+                -(float)((decimal)pt.Y * scaleRate + 3M), 6, 6);
+
+            SizeF pointSize = g.MeasureString($"({pt.X},{pt.Y})", fontSmall);
+            g.DrawString($"({pt.X:0.##},{pt.Y:0.##})", fontSmall, brushPink,
+                (float)((decimal)pt.X * scaleRate - (decimal)pointSize.Width / 2M),
+                -(float)((decimal)pt.Y * scaleRate +
+                    ((pt.Y > 0) ? (decimal)pointSize.Height + 5M: (decimal)-pointSize.Height + 5M)));
+
+            if (withLine)
+            {
+                penBlue.DashStyle = DashStyle.Dash;
+                penBlue.SetLineCap(LineCap.Flat, LineCap.Flat, DashCap.Flat);
+                g.DrawLine(penBlue,
+                    (float)((decimal)pt.X * scaleRate), (float)((decimal)-pt.Y * scaleRate),
+                    0f, (float)((decimal)-pt.Y * scaleRate));
+
+                g.DrawLine(penBlue,
+                    (float)((decimal)pt.X * scaleRate), (float)((decimal)-pt.Y * scaleRate),
+                    (float)((decimal)pt.X * scaleRate), 0f);
+
+                if(pt.X != 0)
+                {
+                    SizeF ptXSize = g.MeasureString($"{pt.X}", fontSmall);
+                    g.DrawString($"{pt.X:0.##}", fontSmall, brushPink,
+                        (float)((decimal)pt.X * scaleRate -(decimal)ptXSize.Width / 2M),
+                        (pt.Y > 0) ? 5f : -ptXSize.Height - 5f);
+                }
+
+                if(pt.Y != 0)
+                {
+                    SizeF ptYSize = g.MeasureString($"{pt.Y}", fontSmall);
+                    g.DrawString($"{pt.Y:0.##}", fontSmall, brushPink,
+                        (pt.X > 0) ? -ptYSize.Width -5f : 5f,
+                    -(float)((decimal)pt.Y * scaleRate + (decimal)ptYSize.Height / 2M));
+                }
+            }//if withLine
+
+            brushPink.Dispose();
+        }//DrawPointLine()
+
+        //====== Auto Scale ======
+        public void PointAutoScale(PointF pt, bool autoScale = true)
+        {
+            if(!autoScale) { return; }
+            
+            while ((Math.Abs((decimal)centerPoint.X) / scaleRate) < Math.Abs((decimal)pt.X)
+                || (Math.Abs((decimal)centerPoint.Y) / scaleRate) < Math.Abs((decimal)pt.Y))
+            {
+                SetScaleRate(scaleRate / 2M);
+
+                if(scaleRate < 0.02M) { break; }  
+            }//while
+        }//PointAutoScale()
+
+        public void SetScaleRate(decimal scaleRate)
+        {
+            this.scaleRate = scaleRate;
+            g.Clear(SystemColors.Window);
+            DrawCoordinateAxis();
+        }//SetScaleRate()
+    }//class
+}
---
+++ EquationCircle.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace WinFormGUI.WinFormSample.Viewer.CoordinateAlgorithm
+{
+    class EquationCircle : ICoordinateEquation
+    {
+        public decimal Radius { get; private set; }
+        public decimal RadiusSq { get; private set; }
+        public PointF CircleCenterPoint { get; private set; }
+        public decimal A { get; private set; }
+        public decimal B { get; private set; }
+        public decimal C { get; private set; }
+        public PointF[] InterceptPointX { get; private set; }
+        public PointF[] InterceptPointY { get; private set; }
+        public PointF[] EqPointAry { get; private set; }
+        public string Text { get; set; }
+
+        public EquationCircle(decimal radius, PointF cirleCenterPoint)
+        {
+            // (x - p) ^ 2 + (y - q) ^ 2 = r ^ 2
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Circle radius should be plus value.");
+            }
+
+            this.Radius = radius;
+            this.RadiusSq = radius * radius;
+            this.CircleCenterPoint = cirleCenterPoint;
+            (decimal a, decimal b, decimal c) = 
+                BuildGeneralParameterCircle(Radius, cirleCenterPoint);
+            this.A = a;
+            this.B = b;
+            this.C = c;
+            this.InterceptPointX = AlgoInterceptX();
+            this.InterceptPointY = AlgoInterceptY();
+            this.EqPointAry = BuildEqPointAry();
+            this.Text = BuildText(Radius, cirleCenterPoint);
+        }//constructor
+
+        public EquationCircle(decimal radius, float p, float q)
+            : this(radius, new PointF(p, q)) { }
+
+        //【Deprecated】非推奨: Radius is including Math.Sqrt(double).
+        public EquationCircle(decimal a, decimal b, decimal c)
+        {
+            // x ^ 2 + y ^ 2 + a x + b y + c = 0
+            (decimal radius, float p, float q) = BuildSqureCircle(a, b, c);
+
+            new EquationCircle(radius, new PointF(p, q));
+        }
+
+        private (decimal a, decimal b, decimal c) 
+            BuildGeneralParameterCircle(decimal radius, PointF circleCenterPoint)
+        {
+            // [(x - p) ^ 2 + (y - q) ^ 2 = r ^ 2]  ->  [x ^ 2 + y ^ 2 + a x + b y + c = 0]
+            // x ^ 2 - 2 p x + p ^ 2 + y ^ 2 - 2 q y + q ^ 2 = r ^ 2
+            // x ^ 2 + y ^ 2 - 2 p x - 2 q y + p ^ 2 + q ^ 2 - r ^ 2 = 0
+            decimal p = (decimal)circleCenterPoint.X;
+            decimal q = (decimal)circleCenterPoint.Y;
+            
+            decimal a = -2M * p;
+            decimal b = -2M * q;
+            decimal c = p * p + q * q - radius * radius;
+
+            return (a, b, c);
+        }//BuildGeneralParam()
+
+        private (decimal radius, float p, float q) BuildSqureCircle(
+            decimal a, decimal b, decimal c)
+        {
+            // [x ^ 2 + y ^ 2 + a x + b y + c = 0]  -> Completed Squre [(x - p) ^ 2 + (y - q) ^ 2 = r ^ 2]
+            // [x ^ 2 + 2 (a / 2) x + a ^ 2 / 4]  - a ^ 2 / 4 + [y ^ 2 + 2 (b / 2) y + b ^ 2 / 4] - b ^ 2 / 4 + c = 0
+            // (x + (a / 2)) ^ 2 + (y + (b / 2)) ^ 2 = a ^ 2 / 4 + b ^ 2 / 4 - c
+            // (x - (- a / 2)) ^ 2 + (y - (- b / 2)) ^ 2 = (a ^ 2 + b ^ 2 - 4 c) / 4 
+            float p = (float)(-a / 2M);
+            float q = (float)(-b / 2M);
+            decimal radius = (decimal)Math.Sqrt((double)((a * a + b * b - 4M * c) / 4M));
+
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Circle radius should be plus value from a, b, c.");
+            }
+
+            return (radius, p, q);
+        }//BuildSqureCircle()
+
+        private PointF[] BuildEqPointAry()
+        {
+            List<PointF> pointList = new List<PointF>();
+            pointList.Add(CircleCenterPoint);
+            pointList.AddRange(InterceptPointX);
+            pointList.AddRange(InterceptPointY);
+
+            return pointList.ToArray();
+        }//BuildEqPointAry()
+
+        private PointF[] AlgoInterceptX()
+        {
+            List<PointF> pointList = new List<PointF>();
+
+            if (Math.Abs((decimal)CircleCenterPoint.Y) < Radius)       // d < r
+            {
+                float[] xAry = AlgoFunctionYtoX(y: 0f);      // y = 0 | (x - p) ^ 2 + (y - q) ^ 2 = r ^ 2
+                pointList.Add(new PointF(xAry[0], 0));
+                pointList.Add(new PointF(xAry[1], 0));
+            }
+            else if (Math.Abs((decimal)CircleCenterPoint.Y) == Radius) // d == r
+            {
+                pointList.Add(new PointF(CircleCenterPoint.X, 0)); 
+            }
+
+            return pointList.ToArray();
+        }//AlgoInterceptX()
+
+        private PointF[] AlgoInterceptY()
+        {
+            List<PointF> pointList = new List<PointF>();
+
+            if ((decimal)Math.Abs(CircleCenterPoint.X) < Radius)  // d < r
+            {
+                float[] yAry = AlgoFunctionXtoY(x: 0f); // x = 0 | (x - p) ^ 2 + (y - q) ^ 2 = r ^ 2
+                pointList.Add(new PointF(0, yAry[0]));
+                pointList.Add(new PointF(0, yAry[1]));
+            }
+            else if ((decimal)Math.Abs(CircleCenterPoint.X) == Radius) // d == r
+            {
+                pointList.Add(new PointF(0, CircleCenterPoint.Y));
+            }
+
+            return pointList.ToArray();
+        }//AlgoInterceptY()
+
+        //====== Text ======
+        private string BuildText(decimal radius, PointF circleCenterPoint)
+        {
+            // Circle Equation  (x - p) ^ 2 + (y - q) ^ 2 = r ^ 2 
+            if( radius <= 0)
+            {
+                throw new ArgumentException("Circle should have plus radius.");
+            }
+
+            string pStr = circleCenterPoint.X > 0 ?
+                $"- {circleCenterPoint.X:0.##}" : $"+ {-circleCenterPoint.X:0.##}";
+            string qStr = circleCenterPoint.Y > 0 ?
+                $"- {circleCenterPoint.Y:0.##}" : $"+ {-circleCenterPoint.Y:0.##}";
+
+            if (circleCenterPoint.X == 0 && circleCenterPoint.Y == 0)
+            {
+                return $"x ^ 2 + y ^ 2 = {radius:0.##} ^ 2";
+            }
+            else if (circleCenterPoint.X == 0)
+            { 
+                return $"x ^ 2 + (y {qStr}) ^ 2 = {radius:0.##} ^ 2";
+            }
+            else if (circleCenterPoint.Y == 0)
+            { 
+                return $"(x {pStr}) ^ 2 + y ^ 2 = {radius:0.##} ^ 2"; 
+            }
+
+            return $"(x {pStr}) ^ 2 + (y {qStr}) ^ 2 = {radius:0.##} ^ 2";
+        }//BuildText()
+
+        public override string ToString()
+        {
+            if (Text == null)
+            {
+                this.Text = BuildText(Radius, CircleCenterPoint);
+            }
+
+            return Text;
+        }//ToString()
+
+        //====== Getter for ICoordinateEquation ======
+        public (decimal a, decimal b, decimal c) GetGeneralParameter()
+        {
+            return (this.A, this.B, this.C);
+        }//GetGeneralParam()
+
+        public PointF[] GetEqPointAry()
+        {
+            return EqPointAry;
+        }
+
+        //====== Algo ======
+        public bool CheckOnLine(PointF pt)
+        {
+            // (x - p) ^ 2 + (y - q) ^ 2 = r ^ 2
+            decimal dx = (decimal)pt.X - (decimal)CircleCenterPoint.X;
+            decimal dy = (decimal)pt.Y - (decimal)CircleCenterPoint.Y;
+
+            return dx * dx + dy * dy == RadiusSq;
+        }//CheckOnLine()
+
+        public float[] AlgoFunctionXtoY(float x)
+        {
+            return AlgoCircleFunctionXtoY(x, this);
+        }
+
+        public float[] AlgoFunctionYtoX(float y)
+        {
+            return AlgoCircleFunctionYtoX(y, this);
+        }
+
+        public float[] AlgoCircleFunctionXtoY(float x, EquationCircle eqCircle)
+        {
+            decimal radiusSq = eqCircle.RadiusSq;
+            decimal p = (decimal)eqCircle.CircleCenterPoint.X;
+            decimal q = (decimal)eqCircle.CircleCenterPoint.Y;
+
+            // (x - p) ^ 2 + (y - q) ^ 2 = r ^ 2
+            // y ^ 2 - 2 q y + q ^ 2 + (x - p) ^ 2 - r ^ 2 = 0
+            var eqQuadY = new EquationQuadratic(
+                a: 1M,
+                b: -2M * q,
+                c: q * q + ((decimal)x - p) * ((decimal)x - p) - radiusSq
+            );
+
+            return eqQuadY.AlgoQuadSolutionFormula();
+        }//AlgoCircleFunctionXtoY()
+
+        public float[] AlgoCircleFunctionYtoX(float y, EquationCircle eqCircle)
+        {
+            decimal radiusSq = eqCircle.RadiusSq;
+            decimal p = (decimal)eqCircle.CircleCenterPoint.X;
+            decimal q = (decimal)eqCircle.CircleCenterPoint.Y;
+
+            // (x - p) ^ 2 + (y - q) ^ 2 = r ^ 2
+            // x ^ 2 - 2 p x + p ^ 2 + (y - q) ^ 2 - r ^ 2 = 0
+            var eqQuadX = new EquationQuadratic(
+                a: 1M,
+                b: -2M * p,
+                c: p * p + ((decimal)y - q) * ((decimal)y - q) - radiusSq
+            );
+
+            return eqQuadX.AlgoQuadSolutionFormula();
+        }//AlgoCircleFunctionYtoX()
+
+        public int AlgoJudgeCircle(PointF pt)
+        {
+            decimal dx = (decimal)CircleCenterPoint.X - (decimal)pt.X;
+            decimal dy = (decimal)CircleCenterPoint.Y - (decimal)pt.Y;
+            decimal distanceSqure = dx * dx + dy * dy;
+
+            int solutionNum = -1;
+            if (distanceSqure > RadiusSq) { solutionNum = 0; }
+            if (distanceSqure == RadiusSq) { solutionNum = 1; }
+            if (distanceSqure < RadiusSq) { solutionNum = 2; }
+
+            return solutionNum;
+        }//AlgoJudgeCircle()
+    }//class
+}
