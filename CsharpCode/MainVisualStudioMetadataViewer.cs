bee2d9d0260ae784c4450fc502757a8ddc4d0988
Shika-Sophia <64418858+shika-sophia@users.noreply.github.com>
2022-12-05 15:54:56 +0900

    2022-12-05-1600

--- CsharpCode/MainVisualStudioMetadataViewer.cs
+++ CsharpCode/MainVisualStudioMetadataViewer.cs
@@ -54,9 +54,9 @@ namespace WinFormGUI.CsharpCode
 {
     class MainVisualStudioMetadataViewer
     {
-        //[STAThread]
-        //static void Main()
-        public void Main()
+        [STAThread]
+        static void Main()
+        //public void Main()
         {
             Console.WriteLine("new FormVisualStudioMetadataViewer()");
 
@@ -310,6 +310,7 @@ namespace WinFormGUI.CsharpCode
                             break;
                         }
                     }
+                    else if (trimedLine.Contains("this[")) { ; }
                     else if (trimedLine.Contains("static readonly"))
                     {
                         if (i == 3 && CaseGenericOverTwo(ref i, splitedWord, className, ref bld))
